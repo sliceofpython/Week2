8022f5e475a5ac54d292d8ad698b9d5f0ece71c2
alpen0 <carsonbirth@gmail.com>
2016-01-19 02:35:52 +0100

    Update alpen homework-2.py

--- alpen homework-2.py
+++ alpen homework-2.py
@@ -1,37 +1,45 @@
 """
-More changes to come, quick game before bed
-EDIT: changed human(), now excepts other answers and re asks.
+alpen
+EDIT1: changed human(), now excepts other answers and re asks.
+EDIT2: gametype added, bof 1, 3, 5
 """
 import random
-print('Lets play a game...\n......\n......\nROCK!\nPAPER!\nSCISSOR!')
-
-def comp():
-    picks = random.choice(['rock', 'paper', 'scissor'])
-    if picks == 'rock':
-        print('RPSMaster5000 picked Rock')
-    elif picks == 'paper':
-        print('RPSMaster5000 picked Paper')
-    elif picks == 'scissor':
-        print('RPSMaster5000 picked Scissor')
-    return picks
-def human():
-    choices = {'rock', 'paper', 'scissor'}
-    picks2 = input('Wh4t d0 yOU p1CK $#up!D Hum4N? ').lower()
-    while picks2 not in choices:
-        print('WE ARE PLAYING ROCK PAPER SCISSOR HUMAN!')
-        picks2 = input('PICK AGAIN!: ').lower()
-    return picks2
-def counter():
-    picks2 = human()
-    picks = comp()
-    if picks == picks2:
-        print('Tie!')
-    elif picks == 'rock' and picks2 == 'scissor':
-        print('HAHA GET R3KT N00B!')
-    elif picks == 'paper' and picks2 == 'rock':
-        print('LOL SO EASY!')
-    elif picks == 'scissor' and picks2 == 'paper':
-        print('I ALWAYS WIN! GIVE UP!')
+print('LETS PLAY A GAME!\n..........\nROCK!\nPAPER!\nSCISSOR!')
+swag = {'paper':1, 'rock':2, 'scissor':3}
+def gametype():
+    gamepick = (input('Best of 1, 3, or 5? '))
+    gtype = {'1', '3', '5'}
+    while gamepick not in gtype:
+        gamepick = (input('1, 3, or 5. '))
+    return gamepick
+def inp():
+    humanpick = input('WH4T D0 Y0U P!CK $#UP1D HUM4N? ').lower()
+    choices = {'paper', 'rock', 'scissor'}
+    while humanpick not in choices:
+        humanpick = input('REALLY?? PICK AGAIN! ').lower()
+    return swag[humanpick]
+def pcinp():
+    comppick = random.choice(['rock', 'paper', 'scissor'])
+    print('RPSMaster5000 picks '+comppick)
+    return swag[comppick]
+def decide():
+    bof = int(gametype())//2
+    hpoints = 0
+    pcpoints = 0
+    while hpoints != bof+1 and pcpoints != bof+1:
+        hpick = inp()
+        pcpick = pcinp()
+        if pcpick < hpick:
+            pcpoints += 1
+        elif pcpick > hpick:
+            hpoints += 1
+        else:
+            pcpoints += 0
+    return hpoints, pcpoints
+def whowin():
+    human, comp = decide()
+    if human > comp:
+        print('THAT WAS ALL LUCK!!')
     else:
-        print('LUCKY PICK HUMAN!')
-counter()
+        print('HAHAHA I NEVER LOSE!!')
+whowin()
